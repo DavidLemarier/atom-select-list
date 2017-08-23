1c2350000df26e68f902e4f538a239026639071e
David Lemarier <david@lemarier.ca>
2017-08-23 14:03:31 -0400

    rename

--- src/select-list-view.js
+++ src/select-list-view.js
@@ -1,387 +1,409 @@
-const {Disposable, CompositeDisposable, TextEditor} = require('atom')
-const etch = require('etch')
-const $ = etch.dom
-const fuzzaldrin = require('fuzzaldrin')
-const path = require('path')
+const { Disposable, CompositeDisposable, TextEditor } = require("soldat");
+const etch = require("etch");
+const $ = etch.dom;
+const fuzzaldrin = require("fuzzaldrin");
+const path = require("path");
 
 module.exports = class SelectListView {
-  constructor (props) {
-    this.props = props
-    this.computeItems(false)
-    this.disposables = new CompositeDisposable()
-    etch.initialize(this)
-    this.element.classList.add('select-list')
-    this.disposables.add(this.refs.queryEditor.onDidChange(this.didChangeQuery.bind(this)))
+  constructor(props) {
+    this.props = props;
+    this.computeItems(false);
+    this.disposables = new CompositeDisposable();
+    etch.initialize(this);
+    this.element.classList.add("select-list");
+    this.disposables.add(
+      this.refs.queryEditor.onDidChange(this.didChangeQuery.bind(this))
+    );
     if (!props.skipCommandsRegistration) {
-      this.disposables.add(this.registerAtomCommands())
+      this.disposables.add(this.registerAtomCommands());
     }
-    const editorElement = this.refs.queryEditor.element
-    const didLoseFocus = this.didLoseFocus.bind(this)
-    editorElement.addEventListener('blur', didLoseFocus)
-    this.disposables.add(new Disposable(() => { editorElement.removeEventListener('blur', didLoseFocus) }))
+    const editorElement = this.refs.queryEditor.element;
+    const didLoseFocus = this.didLoseFocus.bind(this);
+    editorElement.addEventListener("blur", didLoseFocus);
+    this.disposables.add(
+      new Disposable(() => {
+        editorElement.removeEventListener("blur", didLoseFocus);
+      })
+    );
   }
 
-  focus () {
-    this.refs.queryEditor.element.focus()
+  focus() {
+    this.refs.queryEditor.element.focus();
   }
 
-  didLoseFocus (event) {
+  didLoseFocus(event) {
     if (this.element.contains(event.relatedTarget)) {
-      this.refs.queryEditor.element.focus()
+      this.refs.queryEditor.element.focus();
     } else if (document.hasFocus()) {
-      this.cancelSelection()
+      this.cancelSelection();
     }
   }
 
-  reset () {
-    this.refs.queryEditor.setText('')
+  reset() {
+    this.refs.queryEditor.setText("");
   }
 
-  destroy () {
-    this.disposables.dispose()
-    return etch.destroy(this)
+  destroy() {
+    this.disposables.dispose();
+    return etch.destroy(this);
   }
 
-  registerAtomCommands () {
+  registerAtomCommands() {
     return global.atom.commands.add(this.element, {
-      'core:move-up': (event) => {
-        this.selectPrevious()
-        event.stopPropagation()
+      "core:move-up": event => {
+        this.selectPrevious();
+        event.stopPropagation();
       },
-      'core:move-down': (event) => {
-        this.selectNext()
-        event.stopPropagation()
+      "core:move-down": event => {
+        this.selectNext();
+        event.stopPropagation();
       },
-      'core:move-to-top': (event) => {
-        this.selectFirst()
-        event.stopPropagation()
+      "core:move-to-top": event => {
+        this.selectFirst();
+        event.stopPropagation();
       },
-      'core:move-to-bottom': (event) => {
-        this.selectLast()
-        event.stopPropagation()
+      "core:move-to-bottom": event => {
+        this.selectLast();
+        event.stopPropagation();
       },
-      'core:confirm': (event) => {
-        this.confirmSelection()
-        event.stopPropagation()
+      "core:confirm": event => {
+        this.confirmSelection();
+        event.stopPropagation();
       },
-      'core:cancel': (event) => {
-        this.cancelSelection()
-        event.stopPropagation()
+      "core:cancel": event => {
+        this.cancelSelection();
+        event.stopPropagation();
       }
-    })
+    });
   }
 
-  update (props = {}) {
-    let shouldComputeItems = false
+  update(props = {}) {
+    let shouldComputeItems = false;
 
-    if (props.hasOwnProperty('items')) {
-      this.props.items = props.items
-      shouldComputeItems = true
+    if (props.hasOwnProperty("items")) {
+      this.props.items = props.items;
+      shouldComputeItems = true;
     }
 
-    if (props.hasOwnProperty('maxResults')) {
-      this.props.maxResults = props.maxResults
-      shouldComputeItems = true
+    if (props.hasOwnProperty("maxResults")) {
+      this.props.maxResults = props.maxResults;
+      shouldComputeItems = true;
     }
 
-    if (props.hasOwnProperty('filter')) {
-      this.props.filter = props.filter
-      shouldComputeItems = true
+    if (props.hasOwnProperty("filter")) {
+      this.props.filter = props.filter;
+      shouldComputeItems = true;
     }
 
-    if (props.hasOwnProperty('filterQuery')) {
-      this.props.filterQuery = props.filterQuery
-      shouldComputeItems = true
+    if (props.hasOwnProperty("filterQuery")) {
+      this.props.filterQuery = props.filterQuery;
+      shouldComputeItems = true;
     }
 
-    if (props.hasOwnProperty('query')) {
+    if (props.hasOwnProperty("query")) {
       // Items will be recomputed as part of the change event handler, so we
       // don't need to recompute them again at the end of this function.
-      this.refs.queryEditor.setText(props.query)
-      shouldComputeItems = false
+      this.refs.queryEditor.setText(props.query);
+      shouldComputeItems = false;
     }
 
-    if (props.hasOwnProperty('order')) {
-      this.props.order = props.order
+    if (props.hasOwnProperty("order")) {
+      this.props.order = props.order;
     }
 
-    if (props.hasOwnProperty('emptyMessage')) {
-      this.props.emptyMessage = props.emptyMessage
+    if (props.hasOwnProperty("emptyMessage")) {
+      this.props.emptyMessage = props.emptyMessage;
     }
 
-    if (props.hasOwnProperty('errorMessage')) {
-      this.props.errorMessage = props.errorMessage
+    if (props.hasOwnProperty("errorMessage")) {
+      this.props.errorMessage = props.errorMessage;
     }
 
-    if (props.hasOwnProperty('infoMessage')) {
-      this.props.infoMessage = props.infoMessage
+    if (props.hasOwnProperty("infoMessage")) {
+      this.props.infoMessage = props.infoMessage;
     }
 
-    if (props.hasOwnProperty('loadingMessage')) {
-      this.props.loadingMessage = props.loadingMessage
+    if (props.hasOwnProperty("loadingMessage")) {
+      this.props.loadingMessage = props.loadingMessage;
     }
 
-    if (props.hasOwnProperty('loadingBadge')) {
-      this.props.loadingBadge = props.loadingBadge
+    if (props.hasOwnProperty("loadingBadge")) {
+      this.props.loadingBadge = props.loadingBadge;
     }
 
-    if (props.hasOwnProperty('itemsClassList')) {
-      this.props.itemsClassList = props.itemsClassList
+    if (props.hasOwnProperty("itemsClassList")) {
+      this.props.itemsClassList = props.itemsClassList;
     }
 
     if (shouldComputeItems) {
-      this.computeItems()
+      this.computeItems();
     }
 
-    return etch.update(this)
+    return etch.update(this);
   }
 
-  render () {
+  render() {
     return $.div(
       {},
-      $(TextEditor, {ref: 'queryEditor', mini: true}),
+      $(TextEditor, { ref: "queryEditor", mini: true }),
       this.renderLoadingMessage(),
       this.renderInfoMessage(),
       this.renderErrorMessage(),
       this.renderItems()
-    )
+    );
   }
 
-  renderItems () {
+  renderItems() {
     if (this.items.length > 0) {
-      const className = ['list-group'].concat(this.props.itemsClassList || []).join(' ')
+      const className = ["list-group"]
+        .concat(this.props.itemsClassList || [])
+        .join(" ");
       return $.ol(
-        {className, ref: 'items'},
+        { className, ref: "items" },
         ...this.items.map((item, index) => {
-          const selected = this.getSelectedItem() === item
+          const selected = this.getSelectedItem() === item;
 
           return $(ListItemView, {
-            element: this.props.elementForItem(item, {selected, index}),
+            element: this.props.elementForItem(item, { selected, index }),
             selected,
             onclick: () => this.didClickItem(index)
-          })
+          });
         })
-      )
+      );
     } else if (!this.props.loadingMessage && this.props.emptyMessage) {
-      return $.span({ref: 'emptyMessage'}, this.props.emptyMessage)
+      return $.span({ ref: "emptyMessage" }, this.props.emptyMessage);
     } else {
-      return ""
+      return "";
     }
   }
 
-  renderErrorMessage () {
+  renderErrorMessage() {
     if (this.props.errorMessage) {
-      return $.span({ref: 'errorMessage'}, this.props.errorMessage)
+      return $.span({ ref: "errorMessage" }, this.props.errorMessage);
     } else {
-      return ''
+      return "";
     }
   }
 
-  renderInfoMessage () {
+  renderInfoMessage() {
     if (this.props.infoMessage) {
-      return $.span({ref: 'infoMessage'}, this.props.infoMessage)
+      return $.span({ ref: "infoMessage" }, this.props.infoMessage);
     } else {
-      return ''
+      return "";
     }
   }
 
-  renderLoadingMessage () {
+  renderLoadingMessage() {
     if (this.props.loadingMessage) {
       return $.div(
-        {className: 'loading'},
-        $.span({ref: 'loadingMessage', className: 'loading-message'}, this.props.loadingMessage),
-        this.props.loadingBadge ? $.span({ref: 'loadingBadge', className: 'badge'}, this.props.loadingBadge) : ''
-      )
+        { className: "loading" },
+        $.span(
+          { ref: "loadingMessage", className: "loading-message" },
+          this.props.loadingMessage
+        ),
+        this.props.loadingBadge
+          ? $.span(
+              { ref: "loadingBadge", className: "badge" },
+              this.props.loadingBadge
+            )
+          : ""
+      );
     } else {
-      return ''
+      return "";
     }
   }
 
-  getQuery () {
+  getQuery() {
     if (this.refs && this.refs.queryEditor) {
-      return this.refs.queryEditor.getText()
+      return this.refs.queryEditor.getText();
     } else {
-      return ''
+      return "";
     }
   }
 
-  getFilterQuery () {
-    return this.props.filterQuery ? this.props.filterQuery(this.getQuery()) : this.getQuery()
+  getFilterQuery() {
+    return this.props.filterQuery
+      ? this.props.filterQuery(this.getQuery())
+      : this.getQuery();
   }
 
-  didChangeQuery () {
+  didChangeQuery() {
     if (this.props.didChangeQuery) {
-      this.props.didChangeQuery(this.getFilterQuery())
+      this.props.didChangeQuery(this.getFilterQuery());
     }
 
-    this.computeItems()
+    this.computeItems();
   }
 
-  didClickItem (itemIndex) {
-    this.selectIndex(itemIndex)
-    this.confirmSelection()
+  didClickItem(itemIndex) {
+    this.selectIndex(itemIndex);
+    this.confirmSelection();
   }
 
-  computeItems (updateComponent) {
-    const filterFn = this.props.filter || this.fuzzyFilter.bind(this)
-    this.items = filterFn(this.props.items.slice(), this.getFilterQuery())
+  computeItems(updateComponent) {
+    const filterFn = this.props.filter || this.fuzzyFilter.bind(this);
+    this.items = filterFn(this.props.items.slice(), this.getFilterQuery());
     if (this.props.order) {
-      this.items.sort(this.props.order)
+      this.items.sort(this.props.order);
     }
     if (this.props.maxResults) {
-      this.items = this.items.slice(0, this.props.maxResults)
+      this.items = this.items.slice(0, this.props.maxResults);
     }
 
-    this.selectIndex(0, updateComponent)
+    this.selectIndex(0, updateComponent);
   }
 
-  fuzzyFilter (items, query) {
+  fuzzyFilter(items, query) {
     if (query.length === 0) {
-      return items
+      return items;
     } else {
-      const scoredItems = []
+      const scoredItems = [];
       for (const item of items) {
-        const string = this.props.filterKeyForItem ? this.props.filterKeyForItem(item) : item
-        let score = fuzzaldrin.score(string, query)
+        const string = this.props.filterKeyForItem
+          ? this.props.filterKeyForItem(item)
+          : item;
+        let score = fuzzaldrin.score(string, query);
         if (score > 0) {
-          scoredItems.push({item, score})
+          scoredItems.push({ item, score });
         }
       }
-      scoredItems.sort((a, b) => b.score - a.score)
-      return scoredItems.map((i) => i.item)
+      scoredItems.sort((a, b) => b.score - a.score);
+      return scoredItems.map(i => i.item);
     }
   }
 
-  getSelectedItem () {
-    return this.items[this.selectionIndex]
+  getSelectedItem() {
+    return this.items[this.selectionIndex];
   }
 
-  selectPrevious () {
-    return this.selectIndex(this.selectionIndex - 1)
+  selectPrevious() {
+    return this.selectIndex(this.selectionIndex - 1);
   }
 
-  selectNext () {
-    return this.selectIndex(this.selectionIndex + 1)
+  selectNext() {
+    return this.selectIndex(this.selectionIndex + 1);
   }
 
-  selectFirst () {
-    return this.selectIndex(0)
+  selectFirst() {
+    return this.selectIndex(0);
   }
 
-  selectLast () {
-    return this.selectIndex(this.items.length - 1)
+  selectLast() {
+    return this.selectIndex(this.items.length - 1);
   }
 
-  selectIndex (index, updateComponent = true) {
+  selectIndex(index, updateComponent = true) {
     if (index >= this.items.length) {
-      index = 0
+      index = 0;
     } else if (index < 0) {
-      index = this.items.length - 1
+      index = this.items.length - 1;
     }
 
-    this.selectionIndex = index
+    this.selectionIndex = index;
     if (this.props.didChangeSelection) {
-      this.props.didChangeSelection(this.getSelectedItem())
+      this.props.didChangeSelection(this.getSelectedItem());
     }
 
     if (updateComponent) {
-      return etch.update(this)
+      return etch.update(this);
     } else {
-      return Promise.resolve()
+      return Promise.resolve();
     }
   }
 
-  selectItem (item) {
-    const index = this.items.indexOf(item)
+  selectItem(item) {
+    const index = this.items.indexOf(item);
     if (index === -1) {
-      throw new Error('Cannot select the specified item because it does not exist.')
+      throw new Error(
+        "Cannot select the specified item because it does not exist."
+      );
     } else {
-      return this.selectIndex(index)
+      return this.selectIndex(index);
     }
   }
 
-  confirmSelection () {
-    const selectedItem = this.getSelectedItem()
+  confirmSelection() {
+    const selectedItem = this.getSelectedItem();
     if (selectedItem != null) {
       if (this.props.didConfirmSelection) {
-        this.props.didConfirmSelection(selectedItem)
+        this.props.didConfirmSelection(selectedItem);
       }
     } else {
       if (this.props.didConfirmEmptySelection) {
-        this.props.didConfirmEmptySelection()
+        this.props.didConfirmEmptySelection();
       }
     }
   }
 
-  cancelSelection () {
+  cancelSelection() {
     if (this.props.didCancelSelection) {
-      this.props.didCancelSelection()
+      this.props.didCancelSelection();
     }
   }
-}
+};
 
 class ListItemView {
-  constructor (props) {
-    this.mouseDown = this.mouseDown.bind(this)
-    this.mouseUp = this.mouseUp.bind(this)
-    this.didClick = this.didClick.bind(this)
-    this.selected = props.selected
-    this.onclick = props.onclick
-    this.element = props.element
-    this.element.addEventListener('mousedown', this.mouseDown)
-    this.element.addEventListener('mouseup', this.mouseUp)
-    this.element.addEventListener('click', this.didClick)
+  constructor(props) {
+    this.mouseDown = this.mouseDown.bind(this);
+    this.mouseUp = this.mouseUp.bind(this);
+    this.didClick = this.didClick.bind(this);
+    this.selected = props.selected;
+    this.onclick = props.onclick;
+    this.element = props.element;
+    this.element.addEventListener("mousedown", this.mouseDown);
+    this.element.addEventListener("mouseup", this.mouseUp);
+    this.element.addEventListener("click", this.didClick);
     if (this.selected) {
-      this.element.classList.add('selected')
+      this.element.classList.add("selected");
     }
     this.domEventsDisposable = new Disposable(() => {
-      this.element.removeEventListener('mousedown', this.mouseDown)
-      this.element.removeEventListener('mouseup', this.mouseUp)
-      this.element.removeEventListener('click', this.didClick)
-    })
-    etch.getScheduler().updateDocument(this.scrollIntoViewIfNeeded.bind(this))
+      this.element.removeEventListener("mousedown", this.mouseDown);
+      this.element.removeEventListener("mouseup", this.mouseUp);
+      this.element.removeEventListener("click", this.didClick);
+    });
+    etch.getScheduler().updateDocument(this.scrollIntoViewIfNeeded.bind(this));
   }
 
-  mouseDown (event) {
-    event.preventDefault()
+  mouseDown(event) {
+    event.preventDefault();
   }
 
-  mouseUp () {
-    event.preventDefault()
+  mouseUp() {
+    event.preventDefault();
   }
 
-  didClick (event) {
-    event.preventDefault()
-    this.onclick()
+  didClick(event) {
+    event.preventDefault();
+    this.onclick();
   }
 
-  destroy () {
-    this.element.remove()
-    this.domEventsDisposable.dispose()
+  destroy() {
+    this.element.remove();
+    this.domEventsDisposable.dispose();
   }
 
-  update (props) {
-    this.element.removeEventListener('mousedown', this.mouseDown)
-    this.element.removeEventListener('mouseup', this.mouseUp)
-    this.element.removeEventListener('click', this.didClick)
+  update(props) {
+    this.element.removeEventListener("mousedown", this.mouseDown);
+    this.element.removeEventListener("mouseup", this.mouseUp);
+    this.element.removeEventListener("click", this.didClick);
 
-    this.element.parentNode.replaceChild(props.element, this.element)
-    this.element = props.element
-    this.element.addEventListener('mousedown', this.mouseDown)
-    this.element.addEventListener('mouseup', this.mouseUp)
-    this.element.addEventListener('click', this.didClick)
+    this.element.parentNode.replaceChild(props.element, this.element);
+    this.element = props.element;
+    this.element.addEventListener("mousedown", this.mouseDown);
+    this.element.addEventListener("mouseup", this.mouseUp);
+    this.element.addEventListener("click", this.didClick);
     if (props.selected) {
-      this.element.classList.add('selected')
+      this.element.classList.add("selected");
     }
 
-    this.selected = props.selected
-    this.onclick = props.onclick
-    etch.getScheduler().updateDocument(this.scrollIntoViewIfNeeded.bind(this))
+    this.selected = props.selected;
+    this.onclick = props.onclick;
+    etch.getScheduler().updateDocument(this.scrollIntoViewIfNeeded.bind(this));
   }
 
-  scrollIntoViewIfNeeded () {
+  scrollIntoViewIfNeeded() {
     if (this.selected) {
-      this.element.scrollIntoViewIfNeeded()
+      this.element.scrollIntoViewIfNeeded();
     }
   }
 }
